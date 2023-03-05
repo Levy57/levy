88b372c8f667a8d357f6b815302867e62c752486
s.szematowicz <s.szematowicz@gmail.com>
2023-03-05 18:42:19 +0100

    FunWithEnums

---
+++ FunWithEnums/EmpType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+    /// <summary>
+    /// Najprostszy typ (Typ wyliczeniowy enum)
+    /// domyślnie do przechowywania warotści liczbowej w enum służy int
+    /// </summary>
+    public enum EmpType // : int
+    {
+        Manager,            //0
+        Grunt,              //1
+        Contractor,         //2
+        VicePresident,      //3
+        Worker = 102,       //102
+        Boss                //103
+    }
+
+    /// <summary>
+    /// Kształty - numeracja nie po koleji
+    /// </summary>
+    public enum Shape : byte  //- tu zmieniliśmy na byte czyli 4 razy mniejszy
+    {
+        Squer = 3,      //3
+        Circle = 1,     //1
+        Rectangle = 7   //7
+    }
+}
---
+++ FunWithEnums/Program.cs
@@ -0,0 +1,82 @@
+﻿//utwórz typ
+using FunWithEnums;
+
+var emp = EmpType.Contractor;
+//var emp = EmpType.SalesManager  - błąd - nie ma takiej wartości w EmpType
+//var emp = Contractor - błąd - Wartości Contractor nie została przypisana do wylicznia EmpType
+
+var bonus = AskForBonus(emp);
+Console.WriteLine(bonus);
+
+//Klasa System.Enum
+//Wyświtl bazowy typ wyliczenia
+Console.WriteLine("EmpType uses a {0} for storage", Enum.GetUnderlyingType(emp.GetType())); //Można tak - używamy metody GetType gdy mamy zmienna naszego typu
+Console.WriteLine("Shape uses a {0} for storage", Enum.GetUnderlyingType(typeof(Shape))); //albo typeof(NaszTyp) gdy znamy typ ale nie mamy zmiennej
+
+//Wykrywanie Nazwa / wartość -> Contractor - 2
+Console.WriteLine("emp is a {0} - {1}", emp.ToString(), (int)emp);
+
+//Wbudowane enumy np. DayOfWeek
+Console.WriteLine("\n\t\tWpisz jaki dziś dzień\n");
+PrintDetails();
+var dayOfWeek = TryGetDayOfWeek();
+Console.WriteLine($"Today is: {dayOfWeek}");
+
+DayOfWeek TryGetDayOfWeek()
+{
+    while (true)
+    {
+        object obj;
+        var str = Console.ReadLine();
+        var isDayOfWeek = Enum.TryParse(typeof(DayOfWeek), str, out obj);
+        if (isDayOfWeek)
+        {
+            return (DayOfWeek)obj;
+        }
+        else //else jest tu z logicznego punktu nie potrzebne
+             //ponieważ jak isDayOfNumber jest true to wyjdziemy z funkcji w klauzuli if i tu nie dojdziemy
+             //a jak jest false to i tak byśmy pomineli returna i przesli do tej lini kodu
+             //ale wstawiłęm żeby było bardziej czytelnie (chociasz w przyszłości nie będę bo mniej kodu znaczy lepeij)
+             //“Make everything as simple as possible, but not simpler.” - Albert Einstein.
+        {
+            Console.WriteLine("Nie ma takiej opcji, Wpisz jeszcze raz");
+        }
+    }
+}
+
+void PrintDetails()
+{
+    var array = Enum.GetValues(typeof(DayOfWeek));
+    foreach (var item in array)
+    {
+        Console.WriteLine($"{(int)item} - {item}");
+    }
+}
+
+// Enum jako parametr
+byte AskForBonus(EmpType emp)
+{
+    switch (emp)
+    {
+        case EmpType.Manager:
+            return 70;
+
+        case EmpType.Grunt:
+            return 15;
+
+        case EmpType.Contractor:
+            return 50;
+
+        case EmpType.VicePresident:
+            return 80;
+
+        case EmpType.Worker:
+            return 5;
+
+        case EmpType.Boss:
+            return 100;
+
+        default:
+            return 3;
+    }
+}
